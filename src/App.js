caa443771b7b973c1ff89e0b8c99e649e682da56
karthik-140 <shanigaramkarthik@gmail.com>
2023-02-16 11:50:00 +0530

    adding-pages-folder-for-about-&-home-page-compnt

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ import Header from "./components/Layout/Header";
 import AvailableProducts from './components/products/AvailableProducts';
 import Cart from "./components/Cart/Cart";
 import CartProvider from './components/store/cart-provider';
-import About from './components/AboutPage/About';
+import About from './pages/AboutPage/About';
 import Footer from './components/Layout/Footer';
-import HomePage from './components/HomePage/HomePage';
+import HomePage from './pages/HomePage/HomePage';
 
 
 function App() {
